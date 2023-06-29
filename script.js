6f32deb051e203731f07441a5612029075b9de67
christianarteaga2028 <138074045+christianarteaga2028@users.noreply.github.com>
2023-06-29 09:55:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var firstname = "christian";
+var lastname = "arteaga";
+var numberOfSibilings = "3";
+var hasPets = true;
+
+
+console.log(firstname);
+console.log(lastname);
+console.log(numberOfSibilings);
+console.log(hasPets);
+
+
+
+var number = 1;
+
+while (number < 5){
+    console.log("I don't know");
+    number++;
+}
+
+body.setAttribute("style", "background-color: white");
+
+if (clickCount === 2){
+    
+     body.setAttribute("style", "background-color:");      
+}
